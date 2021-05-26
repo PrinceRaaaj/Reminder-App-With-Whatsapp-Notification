6ea96164e4f2d10fcef72e0a639e0b9f4ffaffb3
Prince Raj <princefebraj08@gmail.com>
2021-05-26 22:38:01 +0530

    Completed reminder app

---
+++ index.js
@@ -0,0 +1,108 @@
+require('dotenv').config()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+//APP config
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+//DB config
+mongoose.connect('mongodb://localhost:27017/reminderAppDB', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, () => console.log("DB connected"))
+const reminderSchema = new mongoose.Schema({
+    reminderMsg: String,
+    remindAt: String,
+    isReminded: Boolean
+})
+const Reminder = new mongoose.model("reminder", reminderSchema)
+
+
+//Whatsapp reminding functionality
+
+setInterval(() => {
+    Reminder.find({}, (err, reminderList) => {
+        if(err) {
+            console.log(err)
+        }
+        if(reminderList){
+            reminderList.forEach(reminder => {
+                if(!reminder.isReminded){
+                    const now = new Date()
+                    if((new Date(reminder.remindAt) - now) < 0) {
+                        Reminder.findByIdAndUpdate(reminder._id, {isReminded: true}, (err, remindObj)=>{
+                            if(err){
+                                console.log(err)
+                            }
+                            const accountSid = process.env.ACCOUNT_SID 
+                            const authToken = process.env.AUTH_TOKEN
+                            const client = require('twilio')(accountSid, authToken); 
+                            client.messages 
+                                .create({ 
+                                    body: reminder.reminderMsg, 
+                                    from: 'whatsapp:+14155238886',       
+                                    to: 'whatsapp:+918888888888' //YOUR PHONE NUMBER INSTEAD OF 8888888888
+                                }) 
+                                .then(message => console.log(message.sid)) 
+                                .done()
+                        })
+                    }
+                }
+            })
+        }
+    })
+},1000)
+;
+
+
+//API routes
+app.get("/getAllReminder", (req, res) => {
+    Reminder.find({}, (err, reminderList) => {
+        if(err){
+            console.log(err)
+        }
+        if(reminderList){
+            res.send(reminderList)
+        }
+    })
+})
+app.post("/addReminder", (req, res) => {
+    const { reminderMsg, remindAt } = req.body
+    const reminder = new Reminder({
+        reminderMsg,
+        remindAt,
+        isReminded: false
+    })
+    reminder.save(err => {
+        if(err){
+            console.log(err)
+        }
+        Reminder.find({}, (err, reminderList) => {
+            if(err){
+                console.log(err)
+            }
+            if(reminderList){
+                res.send(reminderList)
+            }
+        })
+    })
+
+})
+app.post("/deleteReminder", (req, res) => {
+    Reminder.deleteOne({_id: req.body.id}, () => {
+        Reminder.find({}, (err, reminderList) => {
+            if(err){
+                console.log(err)
+            }
+            if(reminderList){
+                res.send(reminderList)
+            }
+        })
+    })
+})
+
+app.listen(9000, () => console.log("Be started"))
---
+++ reminder-app-frontend/src/App.js
@@ -0,0 +1,70 @@
+import './App.css'
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import DateTimePicker from "react-datetime-picker"
+
+function App() {
+
+  const [ reminderMsg, setReminderMsg ] = useState("")
+  const [ remindAt, setRemindAt ] = useState()
+  const [ reminderList, setReminderList ] = useState([])
+
+  useEffect(() => {
+      axios.get("http://localhost:9000/getAllReminder").then( res => setReminderList(res.data))
+  }, [])
+
+  const addReminder = () => {
+      axios.post("http://localhost:9000/addReminder", { reminderMsg, remindAt })
+      .then( res => setReminderList(res.data))
+      setReminderMsg("")
+      setRemindAt()
+  }
+
+  const deleteReminder = (id) => {
+    axios.post("http://localhost:9000/deleteReminder", { id })
+    .then( res => setReminderList(res.data))
+  }
+
+  return (
+    <div className="App">
+      <div className="homepage">
+
+        <div className="homepage_header">
+          <h1>Remind Me 🙋‍♂️</h1>
+          <input type="text" placeholder="Reminder notes here..." value={reminderMsg} onChange={e => setReminderMsg(e.target.value)} />
+          <DateTimePicker 
+            value={remindAt}
+            onChange={setRemindAt}
+            minDate={new Date()}
+            minutePlaceholder="mm"
+            hourPlaceholder="hh"
+            dayPlaceholder="DD"
+            monthPlaceholder="MM"
+            yearPlaceholder="YYYY"
+          />
+          <div className="button" onClick={addReminder}>Add Reminder</div>
+        </div>
+
+
+        <div className="homepage_body">
+          {
+            reminderList.map( reminder => (
+              <div className="reminder_card" key={reminder._id}>
+                <h2>{reminder.reminderMsg}</h2>
+                <h3>Remind Me at:</h3>
+                <p>{String(new Date(reminder.remindAt.toLocaleString(undefined, {timezone:"Asia/Kolkata"})))}</p>
+                <div className="button" onClick={() => deleteReminder(reminder._id)}>Delete</div>
+              </div>
+            ))
+          }
+          
+
+          
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App;
